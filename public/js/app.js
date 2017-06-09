33186d5cba7933719b187b51ff9e4df833427340
Ivan Medina <medinadiazivan@gmail.com>
2017-06-09 11:50:09 -0500

    Moving to express server

---
+++ public/js/app.js
@@ -0,0 +1,36 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function (e) {
+	if (this.readyState === 4) {
+		if (this.status === 200) {
+			var response = JSON.parse(this.response);
+			var pokemons = response.results;
+			// var squads = JSON.parse(this.response);	
+			crearPokemons(pokemons);
+		}
+		
+	}
+};
+
+xhr.open("GET", "http://pokeapi.co/api/v2/pokemon/");
+
+xhr.send();
+
+function crearPokemons(pokemons) {
+	var ul = document.getElementById("pokemons");
+
+	pokemons.forEach(function (pokemon) {
+		var li = document.createElement("li");
+		li.textContent = pokemon.name;
+
+		ul.appendChild(li);
+	});
+}
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public/'));
+
+app.listen(3000, function () {
+	console.log('Servidor escuchando en http://localhost:3000');
+});
+
+
+
+
+
+
+
+
+
+
+
