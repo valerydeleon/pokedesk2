3baea242c6283d44657c50cc2f7dd0228d3c1ab8
Ivan Medina <medinadiazivan@gmail.com>
2017-06-09 12:01:19 -0500

    $.getJSON

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,26 @@
-var xhr = new XMLHttpRequest();
+$.getJSON("http://pokeapi.co/api/v2/pokemon/", 
+	function (response) {
+	var pokemons = response.results;
+	crearPokemons(pokemons);
+});
 
-xhr.onreadystatechange = function (e) {
-	if (this.readyState === 4) {
-		if (this.status === 200) {
-			var response = JSON.parse(this.response);
-			var pokemons = response.results;
+// var xhr = new XMLHttpRequest();
+
+// xhr.onreadystatechange = function (e) {
+// 	if (this.readyState === 4) {
+// 		if (this.status === 200) {
+			// var response = JSON.parse(this.response);
+			// var pokemons = response.results;
 			// var squads = JSON.parse(this.response);	
-			crearPokemons(pokemons);
-		}
+			// crearPokemons(pokemons);
+// 		}
 		
-	}
-};
+// 	}
+// };
 
-xhr.open("GET", "http://pokeapi.co/api/v2/pokemon/");
+// xhr.open("GET", "http://pokeapi.co/api/v2/pokemon/");
 
-xhr.send();
+// xhr.send();
 
 function crearPokemons(pokemons) {
 	var ul = document.getElementById("pokemons");
